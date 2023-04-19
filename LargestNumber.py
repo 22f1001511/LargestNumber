d6d5e804e2f3ef380a746e67fb5dea0790793fde
22f1001511 <131279413+22f1001511@users.noreply.github.com>
2023-04-19 20:36:42 +0530

    Update LargestNumber.py

--- LargestNumber.py
+++ LargestNumber.py
@@ -17,4 +17,4 @@ if(st.button("Let's Find")):
             largest = b
         if c > a and c > b:
             largest = c
-        st.text(largest, "is the largest of three numbers.")
+        st.success(largest, "is the largest of three numbers.")
